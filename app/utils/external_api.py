9a80f97684c056f8b374b903239d59d36ecb5c86
Zhenya <yevgeniy_kulik@list.ru>
2026-05-26 21:55:11 +0300

    feat: add extra functions for external api

--- app/utils/external_api.py
+++ app/utils/external_api.py
@@ -1,5 +1,5 @@
 from app.core.config.config import config
-from typing import Dict
+from typing import Dict, List
 import requests
 
 
@@ -18,6 +18,39 @@ def get_currency_list() -> Dict[str, str]:
         raise ValueError(f"Request failed: {e}")
     except (KeyError, TypeError) as e:
         raise ValueError(f"Invalid API response: {e}")
-    
-if __name__ == "__main__":
-    print(get_currency_list())
+
+
+def convert(amount: int, from_cur: str, to_cur: str) -> float:
+    url = config.api_config.base_url + "/convert"
+    headers = {"apikey": config.api_config.key}
+    params = {"amount": amount, "from": from_cur, "to": to_cur}
+    try:
+        response = requests.request("GET", url, headers=headers, params=params)
+        response.raise_for_status()
+        data = response.json()
+        if data.get("success"):
+            return data["result"]
+        else:
+            raise ValueError("API returned success=False")
+    except requests.exceptions.RequestException as e:
+        raise ValueError(f"Request failed: {e}")
+    except (KeyError, TypeError) as e:
+        raise ValueError(f"Invalid API response: {e}")
+
+
+def live_currency(curr_list: List[str], from_cur: str) -> Dict[str, float]:
+    url = config.api_config.base_url + "/live"
+    headers = {"apikey": config.api_config.key}
+    params = {"currencies": curr_list, "source": from_cur}
+    try:
+        response = requests.request("GET", url, headers=headers, params=params)
+        response.raise_for_status()
+        data = response.json()
+        if data.get("success"):
+            return data["quotes"]
+        else:
+            raise ValueError("API returned success=False")
+    except requests.exceptions.RequestException as e:
+        raise ValueError(f"Request failed: {e}")
+    except (KeyError, TypeError) as e:
+        raise ValueError(f"Invalid API response: {e}")
