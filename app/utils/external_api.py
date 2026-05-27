959be607a0802043c5ff6d541a373c1569fe80d6
Zhenya <yevgeniy_kulik@list.ru>
2026-05-27 12:52:25 +0300

    fix: aync context manager and join currency list by comma

--- app/utils/external_api.py
+++ app/utils/external_api.py
@@ -6,7 +6,7 @@ import httpx
 async def get_currency_list() -> Dict[str, str]:
     url = config.api_config.base_url + "/list"
     headers = {"apikey": config.api_config.key}
-    with httpx.AsyncClient() as client:
+    async with httpx.AsyncClient() as client:
         try:
             response = await client.get(url=url, headers=headers)
             response.raise_for_status()
@@ -25,7 +25,7 @@ async def convert(amount: int, from_cur: str, to_cur: str) -> float:
     url = config.api_config.base_url + "/convert"
     headers = {"apikey": config.api_config.key}
     params = {"amount": amount, "from": from_cur, "to": to_cur}
-    with httpx.AsyncClient() as client:
+    async with httpx.AsyncClient() as client:
         try:
             response = await client.get(url=url, headers=headers, params=params)
             response.raise_for_status()
@@ -43,8 +43,8 @@ async def convert(amount: int, from_cur: str, to_cur: str) -> float:
 async def live_currency(curr_list: List[str], from_cur: str) -> Dict[str, float]:
     url = config.api_config.base_url + "/live"
     headers = {"apikey": config.api_config.key}
-    params = {"currencies": curr_list, "source": from_cur}
-    with httpx.AsyncClient() as client:
+    params = {"currencies": "%2C".join(curr_list), "source": from_cur}
+    async with httpx.AsyncClient() as client:
         try:
             response = await client.get(url=url, headers=headers, params=params)
             response.raise_for_status()
