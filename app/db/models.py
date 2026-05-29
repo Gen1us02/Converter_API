2573d9663f68d182dae502f48b59f807e95ab2ef
Zhenya <yevgeniy_kulik@list.ru>
2026-05-29 13:46:59 +0300

    fix: unique username

--- app/db/models.py
+++ app/db/models.py
@@ -9,5 +9,5 @@ class User(Base):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    username: Mapped[str]
+    username: Mapped[str] = mapped_column(unique=True)
     password: Mapped[str]
